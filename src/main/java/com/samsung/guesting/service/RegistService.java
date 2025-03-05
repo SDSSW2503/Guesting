fcbfec720a78d64bd05fcd1076f386cfc74f0bca
김민협(Minhyeop Kim) <gbgreenbravo@gmail.com>
2025-03-05 17:28:15 +0900

    feat: RegistService 커스텀 예외 처리

--- src/main/java/com/samsung/guesting/service/RegistService.java
+++ src/main/java/com/samsung/guesting/service/RegistService.java
@@ -14,7 +14,8 @@ import com.samsung.guesting.entity.Member;
 import com.samsung.guesting.entity.Regist;
 import com.samsung.guesting.entity.Team;
 import com.samsung.guesting.entity.staticField.Status;
-import com.samsung.guesting.exception.CustomException;
+import com.samsung.guesting.exception.AuthException;
+import com.samsung.guesting.exception.BusinessException;
 import com.samsung.guesting.repository.HouseRepository;
 import com.samsung.guesting.repository.MemberRepository;
 import com.samsung.guesting.repository.RegistRepository;
@@ -33,31 +34,31 @@ public class RegistService {
 	
 	//1. 게스팅 신청 : 상대 팀 아이디를 받아서 게스팅을 신청한다.
 	public Regist requestGuesting(Integer memberId, Integer otherTeamId) {
-		Member member = memberRepository.getByMemberId(memberId).orElseThrow(()->new CustomException("잘못된 접근입니다. 다시 로그인 해주세요."));
+		Member member = memberRepository.getByMemberId(memberId).orElseThrow(()->new AuthException("잘못된 접근입니다. 다시 로그인 해주세요."));
 		Team curTeam = member.getTeam();
 		
-		if (Objects.isNull(curTeam)) throw new RuntimeException("팀 가입 후 이용해주세요.");
+		if (Objects.isNull(curTeam)) throw new BusinessException("팀 가입 후 이용해주세요.");
 				
 		//팀이 존재하는지
 		if(teamRepository.findById(otherTeamId).isEmpty()) {
-			throw new CustomException("존재하지 않는 팀입니다.");
+			throw new BusinessException("존재하지 않는 팀입니다.");
 		}
 		//내가 이미 매칭이 된 상태인지
 		if(registRepository.getMatchedTeam(curTeam.getTeamId()).isPresent()) {
-			throw new CustomException("이미 팀의 매칭이 완료된 상태입니다.");
+			throw new BusinessException("이미 팀의 매칭이 완료된 상태입니다.");
 		}
 		//이미 다른 곳에 요청을 보낸 상태인지
 		if(!registRepository.getSentRegists(curTeam.getTeamId()).isEmpty()) {
-			throw new CustomException("이미 보낸 매칭 신청이 존재합니다.");
+			throw new BusinessException("이미 보낸 매칭 신청이 존재합니다.");
 		}
 		//상대 팀이 이미 매칭이 된 상태인지
 		if(registRepository.getMatchedTeam(otherTeamId).isPresent()) {
-			throw new CustomException("이미 매칭이 완료된 팀입니다.");
+			throw new BusinessException("이미 매칭이 완료된 팀입니다.");
 		}
   
   		//fetch all houses, and select one at random
 		List<House> houseList = houseRepository.findAll(); 
-		if (houseList.isEmpty()) throw new CustomException("empty house DB");
+		if (houseList.isEmpty()) throw new BusinessException("empty house DB");
 		House house = houseList.stream()
 				.skip(new Random().nextInt((int) houseRepository.count()))
 				.findFirst().get();
@@ -76,9 +77,9 @@ public class RegistService {
 	//2. 신청 현황 조회 : 나의 팀이 받은 신청을 조회한다.
 	public List<RegistRes> viewReceivedRegistRecords(Integer memberId) {
 		//1) session 에서 나의 팀 아이디를 꺼낸다.
-		Member member = memberRepository.getByMemberId(memberId).orElseThrow(()->new CustomException("잘못된 접근입니다. 다시 로그인 해주세요."));
+		Member member = memberRepository.getByMemberId(memberId).orElseThrow(()->new AuthException("잘못된 접근입니다. 다시 로그인 해주세요."));
 		Team curTeam = member.getTeam();
-		if (Objects.isNull(curTeam)) throw new RuntimeException("팀 가입 후 이용해주세요.");
+		if (Objects.isNull(curTeam)) throw new BusinessException("팀 가입 후 이용해주세요.");
 		//2) DB를 조회해서 나의 팀이 받은 신청을 모두 꺼낸다.
 		List<Regist> registEntities = registRepository.getReceivedRegists(curTeam.getTeamId());
 		
@@ -91,11 +92,11 @@ public class RegistService {
 	//2.5. 신청 현황 조회 : 나의 팀이 보낸 신청을 조회한다.
 	public List<RegistRes> viewSentRegistRecords(Integer memberId) {
 		//1) session 에서 나의 팀 아이디를 꺼낸다.
-		Member member = memberRepository.getByMemberId(memberId).orElseThrow(()->new CustomException("잘못된 접근입니다. 다시 로그인 해주세요."));
-		if (Objects.isNull(member)) throw new RuntimeException("다시 로그인 해주세요.");
+		Member member = memberRepository.getByMemberId(memberId).orElseThrow(()->new AuthException("잘못된 접근입니다. 다시 로그인 해주세요."));
+		if (Objects.isNull(member)) throw new BusinessException("다시 로그인 해주세요.");
 		Team curTeam = member.getTeam();
 		
-		if (Objects.isNull(curTeam)) throw new RuntimeException("팀 가입 후 이용해주세요.");
+		if (Objects.isNull(curTeam)) throw new BusinessException("팀 가입 후 이용해주세요.");
 
 	  //2) DB를 조회해서 나의 팀이 받은 신청을 모두 꺼낸다.
 		List<Regist> registEntities = registRepository.getSentRegists(curTeam.getTeamId());
@@ -108,20 +109,20 @@ public class RegistService {
 	
 	//3. 내가 받은 신청 중 하나를 수락한다.
 	@Transactional
-	public void acceptRegist(Integer registId, Integer memberId) throws RuntimeException{
-		Member member = memberRepository.getByMemberId(memberId).orElseThrow(()->new CustomException("잘못된 접근입니다. 다시 로그인 해주세요."));
+	public void acceptRegist(Integer registId, Integer memberId) {
+		Member member = memberRepository.getByMemberId(memberId).orElseThrow(()->new AuthException("잘못된 접근입니다. 다시 로그인 해주세요."));
 		Team curTeam = member.getTeam();
 		
-		if (Objects.isNull(curTeam)) throw new RuntimeException("팀 가입 후 이용해주세요.");
+		if (Objects.isNull(curTeam)) throw new BusinessException("팀 가입 후 이용해주세요.");
 
 		Regist regist = registRepository.findById(registId)
-				.orElseThrow(()->new RuntimeException("찾을 수 없는 요청입니다."));
+				.orElseThrow(()->new BusinessException("찾을 수 없는 요청입니다."));
 		
 		if (!Objects.equals(regist.getReceiveTeam().getTeamId(), curTeam.getTeamId()))
-			throw new RuntimeException("자신이 소속된 팀의 게스팅만 수락할 수 있습니다..");
+			throw new BusinessException("자신이 소속된 팀의 게스팅만 수락할 수 있습니다..");
 		//이미 거절된 요청이면 에러 반환
 		if(regist.getStatus().equals(Status.DECLINED)) {
-			throw new CustomException("이미 거절된 요청을 수락할 수 없습니다.");
+			throw new BusinessException("이미 거절된 요청을 수락할 수 없습니다.");
 		}
 		
 		
@@ -142,18 +143,18 @@ public class RegistService {
 	 
 	 //4. 내가 받은 신청 중 하나를 거절한다.
 	@Transactional
-	public void declineRegist(Integer registId, Integer memberId) throws RuntimeException{
-		Member member = memberRepository.getByMemberId(memberId).orElseThrow(()->new CustomException("잘못된 접근입니다. 다시 로그인 해주세요."));
-		if (Objects.isNull(member)) throw new RuntimeException("다시 로그인 해주세요.");
+	public void declineRegist(Integer registId, Integer memberId) {
+		Member member = memberRepository.getByMemberId(memberId).orElseThrow(()->new AuthException("잘못된 접근입니다. 다시 로그인 해주세요."));
+		if (Objects.isNull(member)) throw new AuthException("다시 로그인 해주세요.");
 		Team curTeam = member.getTeam();
 		
-		if (Objects.isNull(curTeam)) throw new RuntimeException("팀 가입 후 이용해주세요.");
+		if (Objects.isNull(curTeam)) throw new BusinessException("팀 가입 후 이용해주세요.");
 		
 		Regist regist = registRepository.findById(registId)
-				.orElseThrow(()->new RuntimeException("찾을 수 없는 요청입니다."));
+				.orElseThrow(()->new BusinessException("찾을 수 없는 요청입니다."));
 		
 		if (!Objects.equals(regist.getReceiveTeam().getTeamId(), curTeam.getTeamId()))
-			throw new RuntimeException("자신이 소속된 팀의 게스팅만 거절할 수 있습니다..");
+			throw new BusinessException("자신이 소속된 팀의 게스팅만 거절할 수 있습니다..");
 
 		//거절로 바꾸기
 		regist.setStatus(Status.DECLINED);
@@ -161,16 +162,16 @@ public class RegistService {
 	 
 	 
 	 //5. 성사된 매칭을 조회한다.
-	public RegistRes getMyMatching (Integer memberId) throws RuntimeException{
-		Member member = memberRepository.getByMemberId(memberId).orElseThrow(()->new CustomException("잘못된 접근입니다. 다시 로그인 해주세요."));
-		if (Objects.isNull(member)) throw new RuntimeException("다시 로그인 해주세요.");
+	public RegistRes getMyMatching (Integer memberId) {
+		Member member = memberRepository.getByMemberId(memberId).orElseThrow(()->new AuthException("잘못된 접근입니다. 다시 로그인 해주세요."));
+		if (Objects.isNull(member)) throw new AuthException("다시 로그인 해주세요.");
 		Team curTeam = member.getTeam();
 		
-		if (Objects.isNull(curTeam)) throw new RuntimeException("팀 가입 후 이용해주세요.");
+		if (Objects.isNull(curTeam)) throw new BusinessException("팀 가입 후 이용해주세요.");
 		
 		//내 팀 아이디를 이용해 성사된 매칭을 조회한다.
 		Regist regist = registRepository.getMatchedTeam(curTeam.getTeamId())
-				.orElseThrow(() -> new RuntimeException("성사된 매칭이 존재하지 않습니다."));
+				.orElseThrow(() -> new BusinessException("성사된 매칭이 존재하지 않습니다."));
 		return new RegistRes(regist);
 	}
 }
