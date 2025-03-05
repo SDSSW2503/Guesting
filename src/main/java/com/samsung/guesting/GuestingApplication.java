1c9208efac842642cf9c598ac0a26d640546087e
Sanity1207 <newxxxenson@gmail.com>
2025-03-05 09:52:21 +0900

    feat : added example sql file, added tests to registRepostiory and fixed registRepository

--- src/main/java/com/samsung/guesting/GuestingApplication.java
+++ src/main/java/com/samsung/guesting/GuestingApplication.java
@@ -1,12 +1,16 @@
 package com.samsung.guesting;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.samsung.guesting.dto.MemberRes;
+import com.samsung.guesting.entity.Regist;
 import com.samsung.guesting.repository.MemberRepository;
+import com.samsung.guesting.repository.RegistRepository;
 import com.samsung.guesting.service.MemberService;
 
 @SpringBootApplication
@@ -22,11 +26,29 @@ public class GuestingApplication implements CommandLineRunner{
 	@Autowired
 	private MemberService memberService;
 	
+	@Autowired
+	private RegistRepository registRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
-		MemberRes res = memberService.getMyInfo(1);
-		System.out.println(res.getTeamRes());
+		/*RegistRepository Test*/
 		
+		//getReceivedRegists : 팀이 받은 요청 모두 조회
+		registRepository.getReceivedRegists(1)
+		.stream().forEach(System.out::println);
+		
+		
+		//getSentRegists : 팀이 보낸 요청 모두 조회
+		registRepository.getSentRegists(2)
+		.stream().forEach(System.out::println);
+		
+		//getMatchedTeam : 매칭된 팀 조회
+		//need mechanism to prevent
+			//1. multiple requests coming from me
+			//2. multiple matchings
+		System.out.println("Matched Team : ");
+		registRepository.getMatchedTeam(1) 
+		.stream().forEach(System.out::println);
 	}
 
 }
