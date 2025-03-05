ef41c5b44edd04413b3bf709388287dc8fe2a806
Sanity1207 <newxxxenson@gmail.com>
2025-03-05 13:01:11 +0900

    registService Test complete.

--- src/main/java/com/samsung/guesting/dto/RegistReq.java
+++ src/main/java/com/samsung/guesting/dto/RegistReq.java
@@ -1,9 +1,13 @@
 package com.samsung.guesting.dto;
 
+import java.time.LocalDateTime;
+
+import com.samsung.guesting.entity.House;
 import com.samsung.guesting.entity.Regist;
 import com.samsung.guesting.entity.Team;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,15 +18,18 @@ import lombok.ToString;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
+@Builder
 public class RegistReq {
 	private Integer sendTeamId;
-	private Integer receiveTeamId;	
+	private Integer receiveTeamId;
 	
-	public Regist toRegist() {
+	public Regist toRegist(House house) {
 		return Regist.builder()
 				.sendTeam(Team.builder().teamId(sendTeamId).build())
 				.receiveTeam(Team.builder().teamId(receiveTeamId).build())
 				.status(2)
+				.regDate(LocalDateTime.now())
+				.house(house)
 				.build();
 	}
 }
--- src/main/java/com/samsung/guesting/service/RegistService.java
+++ src/main/java/com/samsung/guesting/service/RegistService.java
@@ -1,6 +1,7 @@
 package com.samsung.guesting.service;
 
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
@@ -8,9 +9,11 @@ import org.springframework.stereotype.Service;
 
 import com.samsung.guesting.dto.RegistReq;
 import com.samsung.guesting.dto.RegistRes;
+import com.samsung.guesting.entity.House;
 import com.samsung.guesting.entity.Regist;
 import com.samsung.guesting.entity.Team;
 import com.samsung.guesting.exception.CustomException;
+import com.samsung.guesting.repository.HouseRepository;
 import com.samsung.guesting.repository.MemberRepository;
 import com.samsung.guesting.repository.RegistRepository;
 import com.samsung.guesting.repository.TeamRepository;
@@ -26,14 +29,18 @@ public class RegistService {
 	private final MemberRepository memberRepository;
 	private final TeamRepository teamRepository;
 	private final HttpSession session;
+	private final HouseRepository houseRepository;
 	
 	//1. 게스팅 신청 : 상대 팀 아이디를 받아서 게스팅을 신청한다.
-	public Regist requestGuesting(RegistReq registReq, Integer curTeamId) {
-		registReq.setSendTeamId(curTeamId); 
+	public Regist requestGuesting(Integer receiveTeamId, Integer curTeamId) {
+		RegistReq registReq = RegistReq.builder()
+				.receiveTeamId(receiveTeamId)
+				.sendTeamId(curTeamId)
+				.build();
 		
 		//TODO : Add error checking mechanism (cannot request to more than one team)
 		//팀이 존재하는지
-		if(teamRepository.findById(registReq.getReceiveTeamId()).isEmpty()) {
+		if(teamRepository.findById(receiveTeamId).isEmpty()) {
 			throw new CustomException("존재하지 않는 팀입니다.");
 		}
 		//내가 이미 매칭이 된 상태인지
@@ -48,14 +55,20 @@ public class RegistService {
 		if(registRepository.getMatchedTeam(registReq.getReceiveTeamId()).isPresent()) {
 			throw new CustomException("이미 매칭이 완료된 팀입니다.");
 		}
-		return registRepository.save(registReq.toRegist());
+		//fetch all houses, and select one at random
+		House house = houseRepository.findAll()
+		.stream()
+		.skip(new Random().nextInt((int) houseRepository.count()))
+		.findFirst()
+		.orElseThrow(()-> new CustomException("empty house DB"));
+		
+		return registRepository.save(registReq.toRegist(house));
 	}
 	 
 	
 	//2. 신청 현황 조회 : 나의 팀이 받은 신청과 나의 팀이 보낸 신청을 조회한다.
-	public List<RegistRes> viewReceivedRegistRecords() {
-		//1) session 에서 나의 팀 아이디를 꺼낸다.
-			Integer curTeamId = (Integer)session.getAttribute("curTeamId");
+	public List<RegistRes> viewReceivedRegistRecords(Integer curTeamId) {
+
 		//2) DB를 조회해서 나의 팀이 받은 신청을 모두 꺼낸다.
 			List<Regist> registEntities = registRepository.getReceivedRegists(curTeamId);
 		
@@ -66,9 +79,7 @@ public class RegistService {
 	}
 	
 	//2.5. 신청 현황 조회 : 나의 팀이 보낸 신청을 조회한다.
-	public List<RegistRes> viewSentRegistRecords(RegistReq registReq) {
-		//1) session 에서 나의 팀 아이디를 꺼낸다.
-		Integer curTeamId = (Integer)session.getAttribute("curTeamId");
+	public List<RegistRes> viewSentRegistRecords(Integer curTeamId) {
 		//2) DB를 조회해서 나의 팀이 받은 신청을 모두 꺼낸다.
 		List<Regist> registEntities = registRepository.getSentRegists(curTeamId);
 	
@@ -80,8 +91,14 @@ public class RegistService {
 	
 	//3. 내가 받은 신청 중 하나를 수락한다.
 	@Transactional
-	public void acceptRegist(RegistReq registReq, Integer curTeamId) throws RuntimeException{
-		Regist regist = registRepository.findById(registReq.getSendTeamId())
+	public void acceptRegist(Integer registId, Integer curTeamId) throws RuntimeException{
+		
+		//내가 이미 매칭이 된 상태인지
+		if(registRepository.getMatchedTeam(curTeamId).isPresent()) {
+			throw new CustomException("이미 팀의 매칭이 완료된 상태입니다.");
+		}
+
+		Regist regist = registRepository.findById(registId)
 				.orElseThrow(()->new RuntimeException("찾을 수 없는 요청입니다."));
 		
 		//수락으로 바꾸기
@@ -100,8 +117,8 @@ public class RegistService {
 	 
 	 //4. 내가 받은 신청 중 하나를 거절한다.
 	@Transactional
-	public void declineRegist(RegistReq registReq, Integer curTeamId) throws RuntimeException{
-		Regist receivedRegist = registRepository.findById(registReq.getSendTeamId())
+	public void declineRegist(Integer registReqId, Integer curTeamId) throws RuntimeException{
+		Regist receivedRegist = registRepository.findById(registReqId)
 				.orElseThrow(()->new CustomException("찾을 수 없는 요청입니다."));
 		//거절로 바꾸기
 		receivedRegist.setStatus(1);
@@ -111,7 +128,8 @@ public class RegistService {
 	 //5. 성사된 매칭을 조회한다.
 	public Regist getMyMatching (Integer curTeamId) throws RuntimeException{
 		//내 팀 아이디를 이용해 성사된 매칭을 조회한다.
-		Regist regist = registRepository.getMatchedTeam(curTeamId).orElseThrow();
+		Regist regist = registRepository.getMatchedTeam(curTeamId).orElseThrow(
+				() -> new CustomException("성사된 매칭이 없습니다"));
 		return regist;
 	}
 }
